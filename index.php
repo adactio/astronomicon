a89073cd4fcde4fab958bc49c6a23c2ef7adc965
Jeremy Keith <jeremy@adactio.com>
2013-09-29 00:00:43 -0700

    Logic, captain

--- index.php
+++ index.php
@@ -2,4 +2,55 @@
 
 require_once('init.php');
 
+if (isset($_GET['date'])):
+
+    // Convert the input into a timestamp
+    $timestamp = strtotime($_GET['date']);
+
+    //Loop through the songs
+    $song = array();
+    foreach($_DATA['songs'] as $loopsong):
+        //Convert the date to a timestamp
+        $songdate = strtotime($loopsong['date']);
+        if ($songdate < $timestamp):
+            $song = $loopsong;
+            break;
+        endif;
+    endforeach;
+
+    if (empty($song)):
+        echo "no songs for that date.";
+        exit;
+    endif;
+
+    // Grab the current time as a timestamp
+    $now = time();
+    // Calculate the difference between then and now (in seconds)
+    $elapsed = $now - $timestamp;
+    // Convert the elapsed time into years
+    $lightyears = $elapsed/60/60/24/365;
+    // Calculate the distance in parsecs
+    $parsecs = $lightyears*0.306594845;
+
+    //Loop through the stars
+    $star = array();
+    foreach ($_DATA['stars'] as $loopstar):
+        if ($loopstar['distance'] > $parsecs):
+            $star = $loopstar;
+            break;
+        endif;
+    endforeach;
+
+    if (empty($star)):
+        //Nothing matched.
+        echo 'Nothing matched.';
+        exit;
+    endif;
+
+    echo $song['title'];
+    echo $song['artist'];
+    echo $star['name'];
+
+endif;
+
 ?>
--- init.php
+++ init.php
@@ -8,6 +8,6 @@ $_DATA['stars'] = $cosmos['cosmos']['star'];
 
 $songs = json_decode(file_get_contents('src/songs.json'), true);
 
-$_DATA['songs'] = $songs['table']['songs']['song'];
+$_DATA['songs'] = array_reverse($songs['table']['songs']['song']);
 
 ?>
